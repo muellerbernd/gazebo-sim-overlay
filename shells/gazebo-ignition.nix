4687c7a9391255cc8a5a560bc87439a147604630
Bernd Müller <github@muellerbernd.de>
2023-12-28 23:14:01 +0100

    move shells to seperate folder

---
+++ shells/gazebo-ignition.nix
@@ -0,0 +1,23 @@
+{ pkgs }:
+let
+  nixgl = [ pkgs.nixgl.nixGLIntel ];
+  extra = [ pkgs.gazebo_sim ];
+in
+pkgs.mkShell {
+  name = "Gz sim development";
+  buildInputs =
+    if pkgs.lib.strings.hasInfix "/run/current-system/sw"
+      (builtins.getEnv "NIX-PROFILES") then
+      [ extra ]
+    else [
+      extra
+      nixgl
+    ];
+  # shellHook = ''
+  #   # export GZ_CONFIG_PATH=${pkgs.ignition.msgs}/share/gz
+  # '';
+    # export GZ_CONFIG_PATH=${pkgs.sdformat}/share/gz:${pkgs.ignition.transport}/share/gz:${pkgs.ignition.msgs}/share/gz:${pkgs.ignition.plugin}/share/gz:${pkgs.ignition.fuel-tools}/share/gz:${pkgs.ignition.gui}/share/gz:${pkgs.gazebo_sim}/share/gz
+}
+
+# vim: set ts=2 sw=2:
+
