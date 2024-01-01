00f2d487805b29b955bf5f1eadc29b5e4310a27e
Bernd Müller <github@muellerbernd.de>
2024-01-01 11:33:00 +0100

    shells:ignition:

--- shells/gazebo-ignition.nix
+++ shells/gazebo-ignition.nix
@@ -10,10 +10,11 @@ let
     text = ''
       export GZ_CONFIG_PATH=${pkgs.gazebo_new}/share/gz
       echo "$GZ_CONFIG_PATH"
-
+      export QT_QPA_PLATFORM_PLUGIN_PATH=${pkgs.libsForQt5.qt5.qtbase.bin}/lib/qt-${pkgs.libsForQt5.qt5.qtbase.version}/plugins/platforms
       # gz launch sim.gzlaunch
       # gz physics -h
-      gz gui
+      # gz gui
+      gz sim -g
     '';
   };
 
@@ -29,10 +30,12 @@ pkgs.mkShell {
       # pkgs.gazebo_sim
       # pkgs.gazebo_test
       pkgs.gazebo_new
-      nixgl
+      # nixgl
+      # gz
     ];
   shellHook = ''
     export GZ_CONFIG_PATH=${pkgs.gazebo_new}/share/gz
+      export QT_QPA_PLATFORM_PLUGIN_PATH=${pkgs.libsForQt5.qt5.qtbase.bin}/lib/qt-${pkgs.libsForQt5.qt5.qtbase.version}/plugins/platforms
   '';
   # QT_XCB_GL_INTEGRATION = "none";
   # export GZ_CONFIG_PATH=${pkgs.sdformat}/share/gz:${pkgs.ignition.transport}/share/gz:${pkgs.ignition.msgs}/share/gz:${pkgs.ignition.plugin}/share/gz:${pkgs.ignition.fuel-tools}/share/gz:${pkgs.ignition.gui}/share/gz:${pkgs.gazebo_sim}/share/gz
