f533859dfde78257ca9d93baa353ac40ead5fbff
Bernd Müller <github@muellerbernd.de>
2024-02-20 08:04:25 +0100

    add: libdart

--- pkgs/libdart/default.nix
+++ pkgs/libdart/default.nix
@@ -1,95 +1,41 @@
 { lib
-, mkDerivation
+, stdenv
 , fetchurl
 , cmake
 , pkg-config
-, ronn
-, libGL
-, openal
-, hdf5
-, curl
-, tinyxml
-, tinyxml-2
-, libtar
-, gts
-, libusb1
-, qtbase
-, gdal
-, libuuid
-, graphviz
-, libsForQt5
-, freeimage
-, boost
-, protobuf
-, sdformat_9
-, tbb
-, ogre1_9
-, ffmpeg_5
-, ignition
-, ignition-cmake ? ignition.cmake2
-, ignition-common ? ignition.common3
-, ignition-math ? ignition.math6
-, ignition-transport ? ignition.transport8
-, ignition-msgs ? ignition.msgs5
-, ignition-fuel-tools ? ignition.fuel-tools4
-, bullet
-, withBulletEngineSupport ? false
+, eigen
+, fmt
+, libccd
+, fcl
+, assimp
 }:
 
-mkDerivation rec {
+stdenv.mkDerivation rec {
   pname = "libdart";
   version = "6.13.1";
 
   src = fetchurl {
-    url = "https://github.com/dartsim/${pname}/archive/v${version}.tar.gz";
-    hash = "sha256-fphCwEbJ4HVTVbJ0wkCoq79Olivnznt/WRlOX0tYT0U=";
+    url = "https://github.com/dartsim/dart/archive/v${version}.tar.gz";
+    hash = "sha256-03krYbwqeuZoK22H4JtdReMlywjFUDigHlgojdw9WNg=";
   };
 
-  nativeBuildInputs = [ cmake pkg-config];
+  nativeBuildInputs = [ cmake pkg-config ];
 
-  # buildInputs = [
-  #   libGL
-  #   openal
-  #   hdf5
-  #   curl
-  #   tinyxml
-  #   tinyxml-2
-  #   libtar
-  #   gts
-  #   libusb1
-  #   qtbase
-  #   gdal
-  #   libuuid
-  #   graphviz
-  #   ignition-cmake
-  #   ignition-common
-  #   ignition-msgs
-  #   libsForQt5.qwt
-  # ] ++ lib.optional withBulletEngineSupport bullet;
+  buildInputs = [
+    eigen
+    fmt
+    assimp
+  ];
 
-  # propagatedBuildInputs = [
-  #   freeimage
-  #   boost
-  #   protobuf
-  #   sdformat_9
-  #   tbb
-  #   ogre1_9
-  #   ffmpeg_5
-  #   ignition-math
-  #   ignition-transport
-  #   ignition-msgs
-  #   ignition-fuel-tools
-  # ];
+  propagatedBuildInputs = [
+    libccd
+    fcl
+  ];
 
-  # qtWrapperArgs = [
-  #   # Let the gazebo binary see neighboring binaries.
-  #   # It attempts to run gzclient from PATH.
-  #   "--prefix PATH : ${placeholder "out"}/bin"
-  #
-  #   # Prevent Gazebo from attempting to use Wayland.
-  #   # As is the case with RViz2, OGRE does not yet support it.
-  #   "--set WAYLAND_DISPLAY dummy" # "dummy" is arbitrary - it just doesn't exist.
-  # ];
+  patches = [ ./fix_cmake.patch ];
+  cmakeFlags = [
+    "-DDART_TREAT_WARNINGS_AS_ERRORS='off'"
+  ];
 
   meta = with lib; {
     homepage = "https://dartsim.github.io";
