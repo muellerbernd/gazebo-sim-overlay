4fe7c48e7a1c794cd5075935133a74bb0e94726b
Bernd Müller <github@muellerbernd.de>
2024-03-23 10:43:32 +0100

    test with 23.11 nixpkgs

--- flake.nix
+++ flake.nix
@@ -2,36 +2,40 @@
   description = "Gazebosim overlay for the Nix package manager";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    # nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
     # nixpkgs.url = "github:muellerbernd/nixpkgs/nix-ros-overlay";
 
     flake-utils.url = "github:numtide/flake-utils";
     nixgl.url = "github:guibou/nixGL";
   };
-  outputs = { self, nixpkgs, flake-utils, nixgl, ... }:
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    nixgl,
+    ...
+  }:
     with nixpkgs.lib;
     with flake-utils.lib;
-    eachSystem allSystems
-      (system:
-        let
-          pkgs = import nixpkgs {
-            inherit system;
-            overlays = [ self.overlays.default nixgl.overlay ];
-          };
-        in
-        {
+      eachSystem allSystems
+      (system: let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [self.overlays.default nixgl.overlay];
+        };
+      in {
+        # let pkgs = nixpkgs.outputs.legacyPackages.${system};
+        # packages.ignition-gazebo =
+        #   pkgs.libsForQt5.callPackage ./pkgs/ignition-gazebo { };
+        # packages.default = self.outputs.packages.${system}.ignition-gazebo;
 
-          # let pkgs = nixpkgs.outputs.legacyPackages.${system};
-          # packages.ignition-gazebo =
-          #   pkgs.libsForQt5.callPackage ./pkgs/ignition-gazebo { };
-          # packages.default = self.outputs.packages.${system}.ignition-gazebo;
-
-          devShells = {
-            default = import ./shells/gazebo-ignition.nix { inherit pkgs; };
-            classic = import ./shells/gazebo-classic.nix { inherit pkgs; };
-          };
-        }) // {
-      overlays.default = import ./overlay.nix;
-    };
+        devShells = {
+          default = import ./shells/gazebo-ignition.nix {inherit pkgs;};
+          classic = import ./shells/gazebo-classic.nix {inherit pkgs;};
+        };
+      })
+      // {
+        overlays.default = import ./overlay.nix;
+      };
 }
-
